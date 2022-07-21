c51e551b3bd90b45fe55c105f2c10d6c5a05e1a9
prethelalam <prethelalam@gmail.com>
2022-07-21 18:36:40 -0500

    Uploading updated candy store manager program

--- CandyStoreManager.cpp
+++ CandyStoreManager.cpp
@@ -60,7 +60,6 @@ using namespace std;
 // 23.41
 // false
 void StoreCustomerInfo(string Name, char Sex, float Weight, int Age, int Money) {
-
     cout << "Inside Store Customer Info.\n";
     cout << "The name you provided is: " << Name << endl;
     cout << "The sex you provided is: " << Sex << endl;
@@ -68,6 +67,7 @@ void StoreCustomerInfo(string Name, char Sex, float Weight, int Age, int Money) 
     cout << "The age you provided is: " << Age << endl;
     cout << "The money you spent is: " << Money << endl;
     return;
+    
 }
 
 void GiveMeAnInteger(int my_integer) { //functions allow you to do some small behaviors, this jsut shows how to pass information. so this my_integer is like a placeholder where you dont have to keep creating changing the info manually each time. you can even put pancakes, or dogs instead of my_integer
@@ -186,16 +186,43 @@ int main() {
     cout << "I'm about to call GiveMeAnInteger:\n";
     GiveMeAnInteger(10); //this 10 is the integer on line 60. on line 159 i am doing a call to give the function GiveMeAnInteger a integer of 201. so, my integer inside the function has the value of 201 (so youre replacing my integer with 201, so my_integer is like a placeholder). so my_integer is like a varible naem, i even replaced it wiht pancakes and it still worked. the technical term for this is a parameter
     int myAge = 17;
+    cout << "\n";
+
     GiveMeAnInteger(myAge); //so notice how this this is myage = 23. the my_integer on line 60 is jsut a place holder, so since you you pass myAge to the givemeaninteger function, it will call up myage
     int temp = 12321;
+    cout << "\n";
+
     GiveMeAnInteger(temp);
     //parameter: 
+    cout << "\n";
 
     GiveMeMultipleThings(123, 'M', "Hey I'm the string");
+    cout << "\n";
     
 
+
     StoreCustomerInfo("Prethel Alam", 'M', 156, 23, 100);
+    fstream customer; //how to create the file
+    customer.open("customer.txt", ios::out); //this opens up a file to perform write operation using file object. ios::out means to allow output (write operations) to a stream
+    if (customer.is_open()) //checking to see if the file is open
+    {
+    customer << "Inside Store Customer Info."; 
+    customer << "The name you provided is: ";
+    customer << "The sex you provided is: ";
+    customer << "The Weight you provided is: ";
+    customer << "The age you provided is: ";
+    customer << "The money you spent is: ";
+    
+    customer.close(); // this closes the file
+    }
+    customer.open("customer.txt", ios::in); // this ios::in allows the input (read operations) from a stream
+    if (customer.is_open()) //checking if file is open 
+    {
+        string StoreCustomerInfo; //this is putting the function into strings im pretty sure
+        while(getline(customer,StoreCustomerInfo)) //this is reading data from the file object and putting it into a string
+        cout << StoreCustomerInfo << endl; // printing the data of the string
+    }
+    customer.close(); // closing the file
 
 
-    return 0;
 }
